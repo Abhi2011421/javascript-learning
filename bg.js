3ba51e34af141039c9e50f0e5101ea093d6dfb59
Abhi2011421 <9636647316abhi@gmail.com>
2024-11-19 15:26:46 +0530

    bg

---
+++ bg.js
@@ -0,0 +1,11 @@
+document.getElementById('redBtn').addEventListener('click', function () {
+    document.body.style.backgroundColor = 'red';
+});
+
+document.getElementById('blueBtn').addEventListener('click', function () {
+    document.body.style.backgroundColor = 'blue';
+});
+
+document.getElementById('greenBtn').addEventListener('click', function () {
+    document.body.style.backgroundColor = 'green';
+});
